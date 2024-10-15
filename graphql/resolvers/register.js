9e10ec9e9d1e5481144ded8ec0f02b9bc7e6bb02
daniel-samuel <folarinded@gmail.com>
2024-10-15 13:59:46 +0100

    happy

--- graphql/resolvers/register.js
+++ graphql/resolvers/register.js
@@ -1,4 +1,5 @@
 const User = require("../../models/User");
+const JWT = require("../../helpers/jwt");
 
 module.exports = {
   Mutation: {
@@ -11,6 +12,52 @@ module.exports = {
         console.error("Error creating register:", error);
         throw new Error("Failed to create register");
       }
+    },
+
+    async login(_, { input }) {
+      const { number, name } = input;
+
+      const user = await User.Register.findOne({ number, name });
+      if (!user) {
+        throw new Error("Invalid credentials");
+      }
+
+      // Issue a JWT using the user's id and name
+      const token = JWT.issueJwt(user.id.toString(), "1h"); // You can set expiration as needed
+
+      return {
+        token, // Return the token
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          number: user.number,
+          role: user.role,
+          company: user.company, // Include other user details if needed
+          // Add any other fields you want to return
+        },
+      };
+    },
+
+    async createCommitment(_, { input }) {
+      // Verify that the register exists
+      const register = await User.Register.findById(input.register);
+      console.log(register);
+      if (!register) {
+        throw new Error("Register not found");
+      }
+
+      // Create and save the commitment
+      const commitment = new User.Commitment({
+        register: register._id,
+        message: input.message,
+      });
+      await commitment.save();
+
+      // Populate the register field
+      const populatedCommitment = await commitment.populate("register");
+
+      return populatedCommitment;
     },
   },
 
@@ -33,6 +80,29 @@ module.exports = {
         console.error("Error listing registers:", error);
         throw new Error("Failed to list registers");
       }
+    },
+
+    async getCommitmentById(_, { id }) {
+      const commitment = await User.Commitment.findById(id).populate(
+        "register"
+      );
+      if (!commitment) {
+        throw new Error("Commitment not found");
+      }
+      return commitment;
+    },
+
+    async getCommitmentsByRegister(_, { register }) {
+      const registerExists = await User.Register.findById(register);
+      if (!registerExists) {
+        throw new Error("Register entry not found");
+      }
+
+      const commitments = await User.Commitment.find({ register }).populate(
+        "registerId"
+      );
+
+      return commitments;
     },
   },
 };
