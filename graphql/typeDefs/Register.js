a1908c741db8df1ba722c6f9c41c1a39c518b625
daniel-samuel <folarinded@gmail.com>
2024-10-15 13:59:59 +0100

    happy

--- graphql/typeDefs/Register.js
+++ graphql/typeDefs/Register.js
@@ -1,6 +1,7 @@
 const { gql } = require("apollo-server-express");
 const Register = gql`
   type Register {
+    id: ID!
     name: String
     number: String
     email: String
@@ -16,13 +17,39 @@ const Register = gql`
     company: String
   }
 
+  input LoginInput {
+    name: String!
+    number: String!
+  }
+
+  type AuthPayload {
+    token: String
+    user: Register
+  }
+
+  type Commitment {
+    id: ID!
+    register: Register!
+    message: String!
+    committedAt: String!
+  }
+
+  input CommitInput {
+    register: ID!
+    message: String!
+  }
+
   type Query {
     getRegisterByRole(role: String!): [Register]
     listRegisters: [Register]
+    getCommitmentById(id: ID!): Commitment
+    getCommitmentsByRegister(register: ID!): [Commitment!]!
   }
 
   type Mutation {
     createRegister(input: RegisterInput!): Register
+    createCommitment(input: CommitInput!): Commitment
+    login(input: LoginInput!): AuthPayload
   }
 `;
 module.exports = Register;
--- models/User.js
+++ models/User.js
@@ -12,10 +12,22 @@ const register = new Schema({
   email: { type: String, required: true },
 });
 
+const commitmentSchema = new Schema({
+  register: {
+    type: Schema.Types.ObjectId,
+    ref: "REGISTER",
+    required: true,
+  },
+  message: { type: String, required: true },
+  committedAt: { type: Date, default: Date.now },
+});
+
 const Register = model("REGISTER", register);
+const Commitment = model("COMMITMENT", commitmentSchema);
 
 const exportVariables = {
   Register,
+  Commitment,
 };
 
 module.exports = exportVariables;
